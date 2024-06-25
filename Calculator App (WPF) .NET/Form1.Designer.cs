be8e8230ece16206a834a72eb1f23ea0cfbbfb35
umangstha124 <umangstha124@gmail.com>
2024-06-25 15:04:02 +0545

    inital commit

---
+++ Calculator App (WPF) .NET/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Calculator_App__WPF__.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            addBtn = new Button();
+            substractBtn = new Button();
+            multiplyBtn = new Button();
+            divideBtn = new Button();
+            buttonClear = new Button();
+            buttonEqual = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(31, 26);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(626, 119);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(31, 174);
+            button1.Name = "button1";
+            button1.Size = new Size(110, 87);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(156, 174);
+            button2.Name = "button2";
+            button2.Size = new Size(110, 87);
+            button2.TabIndex = 2;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(287, 174);
+            button3.Name = "button3";
+            button3.Size = new Size(110, 87);
+            button3.TabIndex = 3;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(416, 174);
+            button4.Name = "button4";
+            button4.Size = new Size(110, 87);
+            button4.TabIndex = 4;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(547, 174);
+            button5.Name = "button5";
+            button5.Size = new Size(110, 87);
+            button5.TabIndex = 5;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.Location = new Point(31, 284);
+            button6.Name = "button6";
+            button6.Size = new Size(110, 87);
+            button6.TabIndex = 10;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.Location = new Point(156, 284);
+            button7.Name = "button7";
+            button7.Size = new Size(110, 87);
+            button7.TabIndex = 9;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(287, 284);
+            button8.Name = "button8";
+            button8.Size = new Size(110, 87);
+            button8.TabIndex = 8;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.Location = new Point(416, 284);
+            button9.Name = "button9";
+            button9.Size = new Size(110, 87);
+            button9.TabIndex = 7;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button0
+            // 
+            button0.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button0.Location = new Point(547, 284);
+            button0.Name = "button0";
+            button0.Size = new Size(110, 87);
+            button0.TabIndex = 6;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            button0.Click += button0_Click;
+            // 
+            // addBtn
+            // 
+            addBtn.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            addBtn.Location = new Point(31, 388);
+            addBtn.Name = "addBtn";
+            addBtn.Size = new Size(110, 87);
+            addBtn.TabIndex = 15;
+            addBtn.Text = "+";
+            addBtn.UseVisualStyleBackColor = true;
+            addBtn.Click += addBtn_Click;
+            // 
+            // substractBtn
+            // 
+            substractBtn.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            substractBtn.Location = new Point(156, 388);
+            substractBtn.Name = "substractBtn";
+            substractBtn.Size = new Size(110, 87);
+            substractBtn.TabIndex = 14;
+            substractBtn.Text = "-";
+            substractBtn.UseVisualStyleBackColor = true;
+            substractBtn.Click += substractBtn_Click;
+            // 
+            // multiplyBtn
+            // 
+            multiplyBtn.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            multiplyBtn.Location = new Point(287, 388);
+            multiplyBtn.Name = "multiplyBtn";
+            multiplyBtn.Size = new Size(110, 87);
+            multiplyBtn.TabIndex = 13;
+            multiplyBtn.Text = "*";
+            multiplyBtn.UseVisualStyleBackColor = true;
+            multiplyBtn.Click += multiplyBtn_Click;
+            // 
+            // divideBtn
+            // 
+            divideBtn.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            divideBtn.Location = new Point(416, 388);
+            divideBtn.Name = "divideBtn";
+            divideBtn.Size = new Size(110, 87);
+            divideBtn.TabIndex = 12;
+            divideBtn.Text = "/";
+            divideBtn.UseVisualStyleBackColor = true;
+            divideBtn.Click += divideBtn_Click;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonClear.Location = new Point(547, 388);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(110, 87);
+            buttonClear.TabIndex = 11;
+            buttonClear.Text = "Clear";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // buttonEqual
+            // 
+            buttonEqual.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonEqual.Location = new Point(31, 495);
+            buttonEqual.Name = "buttonEqual";
+            buttonEqual.Size = new Size(626, 87);
+            buttonEqual.TabIndex = 16;
+            buttonEqual.Text = "=";
+            buttonEqual.UseVisualStyleBackColor = true;
+            buttonEqual.Click += buttonEqual_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(684, 770);
+            Controls.Add(buttonEqual);
+            Controls.Add(addBtn);
+            Controls.Add(substractBtn);
+            Controls.Add(multiplyBtn);
+            Controls.Add(divideBtn);
+            Controls.Add(buttonClear);
+            Controls.Add(button6);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button0);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Calculator";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button addBtn;
+        private Button substractBtn;
+        private Button multiplyBtn;
+        private Button divideBtn;
+        private Button buttonClear;
+        private Button buttonEqual;
+    }
+}
---
+++ Calculator App (WPF) .NET/Form1.cs
@@ -0,0 +1,124 @@
+namespace Calculator_App__WPF__.NET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string calTotal, option;
+        int num1, num2, result;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button2.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button3.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button5.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button6.Text;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button7.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button8.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button9.Text;
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + button0.Text;
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            option = "+";
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void substractBtn_Click(object sender, EventArgs e)
+        {
+            option = "-";
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void multiplyBtn_Click(object sender, EventArgs e)
+        {
+            option = "*";
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void divideBtn_Click(object sender, EventArgs e)
+        {
+            option = "/";
+            num1 = int.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            result = (0);
+            num1 = (0);
+            num2 = (0);
+
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            num2 = int.Parse(textBox1.Text);
+
+            if (option.Equals("+"))
+                result = num1 + num2;
+
+            if (option.Equals("-"))
+                result = num1 - num2;
+
+            if (option.Equals("*"))
+                result = num1 * num2;
+
+            if (option.Equals("/"))
+                result = num1 / num2;
+
+            textBox1.Text = result + "";
+        }
+    }
+}
